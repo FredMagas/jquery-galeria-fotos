bd67f387b5a04fd8a1127dfa4cabc335d1959dcf
FredMagas <fredericomaga@hotmail.com>
2023-08-25 14:08:18 -0300

    Commit Inicial

---
+++ main.js
@@ -0,0 +1,34 @@
+// $(document).ready(function() {
+//     alert('Ola JQuery')
+// })
+
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown()
+    })
+
+    $('#btn-cancelar').click(function() {
+        $('form').slideUp()
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault()
+
+        const enderecoNovaImagem = $('#endereco-imagem-nova').val()
+        const novoItem = $('<li style="display: none;"></li>')
+
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem)
+        $(`
+        
+            <div class="overlay-imagem-link">
+                <a href="${enderecoNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1500)
+        $('#endereco-imagem-nova').val('')
+    })
+})
